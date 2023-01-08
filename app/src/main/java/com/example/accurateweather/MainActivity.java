e2b4c124a54a130145d2c4493027bbe72016b113
Charan1303 <charan.cherry1303@gmail.com>
2023-01-08 14:13:02 -0500

    Imported List

--- app/src/main/java/com/example/accurateweather/MainActivity.java
+++ app/src/main/java/com/example/accurateweather/MainActivity.java
@@ -24,6 +24,7 @@ import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
