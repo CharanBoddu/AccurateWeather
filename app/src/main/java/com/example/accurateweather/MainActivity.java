177bf5d9dbf9d6163d54d9e503a43e8dc9582dce
Charan1303 <charan.cherry1303@gmail.com>
2023-01-08 14:12:37 -0500

    Checking permissions and getting city name

--- app/src/main/java/com/example/accurateweather/MainActivity.java
+++ app/src/main/java/com/example/accurateweather/MainActivity.java
@@ -7,18 +7,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private RelativeLayout homeRL;
@@ -60,6 +66,31 @@ public class MainActivity extends AppCompatActivity {
         Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
 
+
+    }
+
+    private String getCityName(double longitude, double latitude){
+        String cityName = "Not found";
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+
+        try {
+            List<Address> addresses = gcd.getFromLocation(latitude,longitude,10);
+
+            for(Address adr : addresses){
+                if(adr!= null){
+                    String city = adr.getLocality();
+                    if(city!=null && !city.equals("")){
+                        cityName = city;
+                    }else{
+                        Log.e("TAG","CITY NOT FOUND");
+                        Toast.makeText(this,"User City Not Found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return cityName;
     }
 
     private void getWeatherInfo(String cityName){
